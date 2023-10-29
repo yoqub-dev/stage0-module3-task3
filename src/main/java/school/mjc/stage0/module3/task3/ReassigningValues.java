47d9055491ab2dc69afadc07cd0f5111aa75abdf
yoqub-dev <yoqub.epam@gmail.com>
2023-10-29 20:13:26 +0500

    commit

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, second = 10, third = 100;
+
+        System.out.println("Initial values:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        int linkToFirst = first, linkToSecond = second, linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.println("Values after reassignment:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
